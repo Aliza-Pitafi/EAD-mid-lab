6e65dfc65bef571f5d93551dd60fdb83785c954d
Aliza Pitafi <alizapitafi2022@gmail.com>
2025-03-25 09:40:38 +0500

    added the countDownLightFile

---
+++ CountDownLight.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react'
+
+class CountDownLight extends Component{
+    constructor(){
+        super()
+        this.sate={
+            message:'dark theme'
+        }
+        this.render(){
+            return <div onClick={message}>
+
+            </div>
+        }
+    }
+}
